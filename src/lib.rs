914288a64b6b11e32e55689dc326d50a6adc785b
Raphael Stefanini <raphael@rphlo.com>
2025-07-28 12:57:55 +0300

    Upgrade pyo3

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use pyo3::prelude::*;
-use pyo3::types::{PyList, PyTuple};
 use pyo3::exceptions::PyValueError;
 
 struct DecodingResult {
@@ -35,9 +34,9 @@ fn decode_signed_value_from_string(encoded: &[u8], offset: u32) -> DecodingResul
     }
 }
 
-fn encode_unsigned_number(num: i64) -> Vec<u8>  {
+fn encode_unsigned_number(num: u64) -> Vec<u8>  {
     let mut encoded: Vec<u8> = vec![];
-    let mut tmp: i64 = num;
+    let mut tmp: u64 = num;
     while tmp >= 0x20 {
         encoded.push(((0x20 | (tmp as u8 & 0x1f)) + 63) as u8);
         tmp >>= 5;
@@ -51,7 +50,8 @@ fn encode_signed_number(num: i64) -> Vec<u8> {
     if num < 0 {
         sgn_num = !sgn_num;
     }
-    return encode_unsigned_number(sgn_num);
+    let unsigned_num = sgn_num as u64;
+    return encode_unsigned_number(unsigned_num);
 }
 
 const YEAR2010: i64 = 1262304000;
@@ -86,38 +86,38 @@ fn decode(input: String) -> PyResult<Vec<(i64, f64, f64)>> {
     Ok(output)
 }
 
+
 #[pyfunction]
-fn encode(data: &PyList) -> PyResult<String> {
+fn encode(data: Vec<(f64, f64, f64)>) -> PyResult<String> {
     let mut prev_timestamp: i64 = YEAR2010;
     let mut prev_latitude: f64 = 0.0;
     let mut prev_longitude: f64 = 0.0;
 
     let mut output: Vec<u8> = vec![];
     let mut is_first: bool = true;
-
     for point_object in data.iter() {
-        let point_data = point_object.downcast::<PyTuple>()?;
-        let timestamp: i64 = point_data[0].extract::<f64>()?.round() as i64;
-        let timestamp_diff: i64 = timestamp - prev_timestamp;
+        let point_data = point_object;
+        let timestamp = point_data.0.round() as i64;   
+        let timestamp_diff = timestamp - prev_timestamp; 
         if is_first {
             output.append(&mut encode_signed_number(timestamp_diff));
             is_first = false;
         } else {
-            if timestamp_diff < 0 {
+            if timestamp < prev_timestamp {
                 return Err(PyValueError::new_err("Input data is not sorted"));
             }
-            output.append(&mut encode_unsigned_number(timestamp_diff));
+            output.append(&mut encode_unsigned_number(timestamp_diff as u64));
         }
         
-        let latitude: f64 = point_data[1].extract::<f64>()?;
+        let latitude: f64 = point_data.1;
         let latitude_diff: i64 = ((latitude - prev_latitude) * 1e5).round() as i64;
         output.append(&mut encode_signed_number(latitude_diff));
 
-        let longitude: f64 = point_data[2].extract::<f64>()?;
+        let longitude: f64 = point_data.2;
         let longitude_diff: i64 = ((longitude - prev_longitude) * 1e5).round() as i64;
         output.append(&mut encode_signed_number(longitude_diff));
 
-        prev_timestamp += timestamp_diff;
+        prev_timestamp += timestamp_diff as i64;
         prev_latitude += (latitude_diff as f64) / 1e5;
         prev_longitude += (longitude_diff as f64) / 1e5;
    
@@ -126,8 +126,9 @@ fn encode(data: &PyList) -> PyResult<String> {
 }
 
 #[pymodule]
-fn gps_data_codec(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+fn gps_data_codec(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(encode, m)?)?;
     m.add_function(wrap_pyfunction!(decode, m)?)?;
+
     Ok(())
 }
